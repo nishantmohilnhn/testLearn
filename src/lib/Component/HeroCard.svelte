2c6786c2045cd7454e8ad593efd9fe0f796774db
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-07 14:47:02 +0530

    chang colo

--- src/lib/Component/HeroCard.svelte
+++ src/lib/Component/HeroCard.svelte
@@ -1,4 +1,4 @@
-<div class=" md:flex rounded-xl p-8 md:p-0 dark:bg-slate-800 drop-shadow-xl m-2 bg-gradient-to-r from-cyan-100 to-sky-500">
+<div class=" md:flex rounded-xl p-8 md:p-0 dark:bg-slate-800 drop-shadow-xl m-2 bg-gradient-to-r from-cyan-300 to-violet-300">
    <div class="m-1 rounded-full p-2">
       <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://picsum.photos/384/384" alt="featered pic" width="384" height="384">
    </div>
