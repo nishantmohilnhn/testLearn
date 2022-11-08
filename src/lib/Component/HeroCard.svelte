705547ffb2ce1e5646193b5d55dbc98b8c3d1dd4
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-08 22:07:04 +0530

    gg33

--- src/lib/Component/HeroCard.svelte
+++ src/lib/Component/HeroCard.svelte
@@ -1,11 +1,13 @@
-<div class="flex text-left h-56 bg-blue-600 m-2 rounded-2xl drop-shadow-2xl overflow-clip">
+<div class="flex text-left h-56 bg-blue-400 m-1 drop-shadow-2xl overflow-clip">
    <!----text -->
     <div class="w-1/2 p-4 text-white text-mono ">
-       <h1 class="text-4xl">Manufacturing Machine</h1>
-       <p>Our services will provide you best possible manufacturing unit for your product development</p>
+       <h1 class="sm:text-4xl text-base font-bold ">Manufacturing Machine</h1>
+       <p class=" text-sm md:text-base mt-2 ">Our services will provide you best possible manufacturing unit for your product development.</p>
     </div>
     <!----img-->
- <div class="">
- <img src="https://picsum.photos/265" alt="img">
- </div>
+   <div class=" bg-red-700 container" >
+      <img src="https://picsum.photos/1265" alt="img">
+   </div>
+
+
  </div>
--- src/lib/Component/Nevbare.svelte
+++ src/lib/Component/Nevbare.svelte
@@ -1,18 +1,13 @@
-<header class="bg-gray-900 sm:flex justify-between sm:px-4 sm:py-3">
-	<div class="flex items-center  bg-gray-900 justify-between px-4 py-3 sm:p-0">	
+<header class=" bg-gray-100 sm:flex justify-between sm:px-4 sm:py-3 border-b-4">
+	<div class="flex items-center justify-between  bg-gray-100 px-4 py-3 sm:p-0">	
 		<a href="/">
-		<div class="text-gray-400 flex justify-center items-center text-base hover:text-white font-bold">
-		
-		
-			<svg  viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" h-9">
-			<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6" />
-		  </svg>
-		<p class="pl-2 text-base hover:text-white font-bold">MS Hygiene Solution</p>
+		<div class="flex justify-center items-center text-base text-gray-700 hover:text-black font-bold">
+		<p class="pl-2 text-base hover:text-blue-300 font-bold">MS Hygiene Solution</p>
 		</div>
 	</a>
 	<div class="sm:hidden">
-		<button type="button" class="text-gray-400" on:click={() => isC = !isC} >
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 fill-current hover:text-white">
+		<button type="button" class="text-gray-700" on:click={() => isC = !isC} >
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-7 fill-current hover:text-black">
 				{#if isC}
 				<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
 				{:else}
@@ -24,10 +19,10 @@
 </div>
 	<div  class:hidden ={isC}
 	class="px-2 pt-2 pb-4 sm:flex" >
-		<a href="/about" class="block text-white hover:bg-gray-400 rounded px-2 font-semibold">Product</a>
-		<a href="/about" class="block text-white hover:bg-gray-400 rounded px-2 font-semibold sm:ml-2">Services</a>
-		<a href="/about" class="block text-white hover:bg-gray-400 rounded px-2 font-semibold sm:ml-2">About Us</a>
-		<a href="/about" class="block text-white hover:bg-gray-400 rounded px-2 font-semibold sm:ml-2">Contect Us</a>
+		<a href="/about" class="block text-gray-700 hover:bg-gray-400 rounded px-2 font-semibold">Product</a>
+		<a href="/about" class="block text-gray-700 hover:bg-gray-400 rounded px-2 font-semibold sm:ml-2">Services</a>
+		<a href="/about" class="block text-gray-700 hover:bg-gray-400 rounded px-2 font-semibold sm:ml-2">About Us</a>
+		<a href="/about" class="block text-gray-700 hover:bg-gray-400 rounded px-2 font-semibold sm:ml-2">Contect Us</a>
 	</div>
 </header>
 
