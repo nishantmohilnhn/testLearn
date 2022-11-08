5a46fd78cf36b58050419547cdb169b0f9570df2
nishantmohilnhn <nniisshhaanntt661199@gmail.com>
2022-11-08 18:55:37 +0530

    kk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,10 @@
     <HeroCard/>   
 </div>
 
+<dir class="bg-teal-400 p-4 drop-shadow-md">
+    <p class=" text-center font-mono text-3xl text-white">Product Design</p>
+  
+    <p class="text-mono m-3 text-justify">Will help you to find best suitable product for You. create a new product to be sold by a business to its customers. A very broad coefficient and effective generation and development of ideas through a process that leads to new products. Thus, it is a major aspect of new product development.</p>
+  </dir>
+  
+
